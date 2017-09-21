42f71a47824a915ed1b42eecf79bcd951720e16b
soumyaVPS <soumya_aithal@hotmail.com>
2017-09-21 14:13:48 -0700

    iSubmission 2

--- clone.py
+++ clone.py
@@ -1,67 +1,111 @@
 import csv
+from random import shuffle
+
 import cv2
 import numpy
 
-lines = []
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
-
-images = []
-measurments = []
-for line in lines:
-    source_path=line[0]
-    image = cv2.imread(source_path)
-    #print (image.shape);
-    images.append(image)
-    measurment = float(line[3])
-    measurments.append(measurment)
-
-X_train = numpy.array(images)
-Y_train = numpy.array(measurments)
-
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, K, MaxPooling2D
-
-model = Sequential()
-l1 = Lambda(lambda x: x/255.0 -0.5,input_shape=(160,320,3))
-model.add(l1)
-print ("Lambda shape", l1.output_shape)
-l2 = Convolution2D(6, 5, 5, input_shape=(160, 320, 3),activation=K.relu)
-model.add(l2)
-print ("Conv2D 1 shape", l2.output_shape)
-
-l3=MaxPooling2D((2, 2))
-model.add(l3)
-print ("Pooling shape", l3.output_shape)
-
-l4 = Convolution2D(6, 5, 5, input_shape=(78, 158,6),activation=K.relu)
-model.add(l4)
-print ("Conv2D 2 shape", l4.output_shape)
+import numpy as np
+import sklearn
+import sys
 
 
-l5 = Flatten()
-model.add(l5)
-print ("flatten shape", l5.output_shape)
+def read_csv (dir, samples,type="train") :
+    name = dir + '/driving_log_train.csv'
+    if type=='val':
+        dir + '/driving_log_val.csv'
+    with open(name) as csvfile:
+        reader = list(csv.reader(csvfile))
+
+        for line in reader:
+            line[0] = dir + "/" + line[0].strip()
+            line[1] = dir + "/" + line[1].strip()
+            line[2] = dir + "/" + line[2].strip()
+
+            samples.append(line)
+
+train_samples=[]
+validation_samples = []
+new_model = sys.argv[1]
+c_r_l = sys.argv[2]
 
 
-l6 = Dense(1, activation=K.relu)
-model.add(l6)
-print ("Dense shape", l6.output_shape)
+for dir in sys.argv[2:] :
+    read_csv(dir, train_samples)
+    read_csv(dir, validation_samples)
+print ("training samples", len(train_samples))
+print("validation_samples", len(validation_samples))
+def generator(samples, batch_size=32):
+    global c_r_l
+    num_samples = len(samples)
+
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for line in batch_samples:
+                source_path = line[0]
+                measurment = float(line[3])
+                for c_r_l in range(3):
+                    if c_r_l == 1:
+                        source_path = line[1]
+                        measurment += 0.25
+
+                    elif c_r_l == 2:
+                        source_path = line[2]
+                        measurment -= 0.25
+                    image = cv2.imread(source_path)
+                    img_g = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+                    ch = cv2.split(img_g)
+                    images.append(ch[1])
+                    angles.append(measurment)
+
+                    #flip each image and angle
+                    images.append(cv2.flip(ch[1], 1))
+                    angles.append(measurment * -1.0)
+            X_train = np.array(images)
+            X_train = np.expand_dims(X_train,4)
+            y_train = np.array(angles)
+            yield X_train, y_train
+
+from keras.models import Sequential, load_model
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, K, MaxPooling2D, Dropout, Activation
+
+if new_model == 'new':
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=(160, 320, 1)))
+    model.add(Convolution2D(6, 5, 5, activation="relu", dim_ordering='tf'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(16, 5, 5, activation="relu", dim_ordering='tf'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(84))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+
+else:
+    print("Load old model")
+    model = load_model('model.h5')
 
 
-l7 = Dense(1, activation=K.relu)
-model.add(l7)
-print ("Dense shape", l7.output_shape)
+#model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
 
+    #model.fit_generator(train_generator, samples_per_epoch=len(train_samples),  nb_epoch=7)
 
-l8 = Dense(1, activation=K.relu)
-model.add(l8)
-print ("Dense shape", l8.output_shape)
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
+#batch_size 32*6 192
 
-
-model.compile(loss='mse',optimizer='adam')
-model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit_generator(train_generator,samples_per_epoch=len(train_samples)*6,
+                    validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples)*6,
+                    nb_epoch=2)
 
 model.save('model.h5')
